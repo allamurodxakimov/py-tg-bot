cf4a853e4387ed448b210a2aee9c73f0bddbb682
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-13 16:35:05 +0500

    Bot

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
 from settings import get_token
+from telegram import Bot
 
-print(get_token())
+TOKEN = get_token()
+
+bot = Bot(TOKEN)
+
+last_update = bot.get_updates()[-1]
+
+user = last_update.message.from_user
+message = last_update.message
+
+bot.send_message(
+    chat_id=user.id,
+    text=message.text
+)
--- settings.py
+++ settings.py
@@ -4,4 +4,8 @@ import os
 load_dotenv()
 
 def get_token() -> str:
-    return os.getenv('TOKEN')
+    TOKEN = os.getenv('TOKEN')
+    if not TOKEN:
+        raise ValueError('TOKEN mavjud emas.')
+
+    return TOKEN
